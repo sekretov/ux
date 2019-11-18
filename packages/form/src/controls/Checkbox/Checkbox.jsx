000d478537a91f6889e772e61660fbdc925fc5b7
Alexey <dark.revo4@mail.ru>
2019-11-18 15:11:07 +0400

    @lskjs/form refactored

--- packages/form/src/controls/Checkbox/Checkbox.jsx
+++ packages/form/src/controls/Checkbox/Checkbox.jsx
@@ -1,21 +1,23 @@
-import React from 'react';
+import React, { Fragment } from 'react';
+import { Global } from '@emotion/core';
 import PropTypes from 'prop-types';
 import AntCheckbox from 'antd/lib/checkbox';
-import Label, { globalStyle } from './Checkbox.styles';
+import Label, { globalStyles } from './Checkbox.styles';
 import Bool from './Bool';
 
-globalStyle();
-
 export const BaseCheckbox = ({ onChange, validationState, ...props }) => (
-  <AntCheckbox
-    {...props}
-    onChange={({ target: { checked } }) => onChange(checked)}
-    // onChange={value => onChange(value.target.checked)}
-  >
-    <Label validationState={validationState}>
-      {props.label}
-    </Label>
-  </AntCheckbox>
+  <Fragment>
+    <Global styles={globalStyles} />
+    <AntCheckbox
+      {...props}
+      onChange={({ target: { checked } }) => onChange(checked)}
+      // onChange={value => onChange(value.target.checked)}
+    >
+      <Label validationState={validationState}>
+        {props.label}
+      </Label>
+    </AntCheckbox>
+  </Fragment>
 );
 
 BaseCheckbox.propTypes = {
